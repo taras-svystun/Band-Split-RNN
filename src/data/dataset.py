31254fbc3275c540ac1fe17c1c40aa1c80938a55
taras_svystun <tatarik.sv@gmail.com>
2024-04-15 15:25:08 +0300

    working on remixing

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -38,9 +38,28 @@ class SourceSeparationDataset(Dataset):
         self.sr = sr
 
         if txt_path is None and txt_dir is not None:
-            print('===========I am in the right place============')
             mode = 'train' if self.is_training else 'valid'
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
             self.txt_path = Path(txt_dir) / f"batch_{target}_{mode}.txt"
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
         elif txt_path is not None and txt_dir is None:
             self.txt_path = Path(txt_path)
         else:
@@ -186,7 +205,7 @@ class SourceSeparationDataset(Dataset):
 
         vocals = torch.cat(vocal_samples, 1)
         mix_segment += vocals[:, :mix_segment.shape[1]]
-        
+
         torchaudio.save('../../datasets/tests/vocals.wav', vocals, sr)
         torchaudio.save('../../datasets/tests/mix.wav', mix_segment, sr)
         print()
@@ -265,7 +284,6 @@ class EvalSourceSeparationDataset(Dataset):
 
         # files params
         self.in_fp = Path(in_fp)
-        # print(self.in_fp)
         self.out_fp = Path(out_fp) if out_fp is not None else None
         self.target = target
 
@@ -280,10 +298,29 @@ class EvalSourceSeparationDataset(Dataset):
 
     def get_test_filelist(self) -> tp.List[tp.Tuple[str, str]]:
         filelist = []
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
         test_dir = self.in_fp / 'train'
-
         for fp in test_dir.glob('*Classic*'):
-            print(fp)
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            # print(fp)
             fp_template = str(fp / "{}.wav")
             fp_mix = fp_template.format('mixture')
             fp_tgt = fp_template.format(self.target)
@@ -315,7 +352,6 @@ class EvalSourceSeparationDataset(Dataset):
         return filelist
 
     def load_file(self, file_path: str) -> torch.Tensor:
-        # print(file_path)
         assert Path(file_path).is_file(), f"There is no such file - {file_path}."
         y, sr = torchaudio.load(
             file_path,
