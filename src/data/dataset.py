249e72770af98b120cd05915494e0686ba863e99
taras_svystun <tatarik.sv@gmail.com>
2024-04-18 14:33:08 +0300

    remixing in progress

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -228,7 +228,7 @@ class SourceSeparationDataset(Dataset):
 
 
         vocals = torch.cat(vocal_samples, 1)[:, :mix_segment.shape[1]]
-        SNR = random.uniform(1, 31)
+        SNR = random.uniform(10, 40)
         SNRs = torch.tensor([SNR] * 2)
         
         torchaudio.save(f'../../datasets/tests/mix_{SNR:.1f}.wav', mix_segment, sr)
