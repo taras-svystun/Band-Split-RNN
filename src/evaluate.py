79f527f46e00b39755ee0ee90db44f7bf33c7759
taras_svystun <tatarik.sv@gmail.com>
2024-04-19 12:12:42 +0300

    overfitting 12 sec

--- src/evaluate.py
+++ src/evaluate.py
@@ -57,7 +57,7 @@ class EvaluateProgram:
             # compute and save metrics
             cSDR, uSDR, siSDR = compute_SDRs(y_hat, y_tgt)
             
-            torchaudio.save(f'../../datasets/tests/overfit/mix{round(uSDR[0])}.wav', y, 44100)
+            torchaudio.save(f'../../datasets/tests/overfit/mix{round(uSDR[0])}.wav', y.cpu(), 44100)
             torchaudio.save(f'../../datasets/tests/overfit/actual_vocals{round(uSDR[0])}.wav', y_tgt, 44100)
             torchaudio.save(f'../../datasets/tests/overfit/pred_vocals{round(uSDR[0])}.wav', y_hat, 44100)
 
