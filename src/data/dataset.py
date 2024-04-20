f77c12d203f33379465b1cddb0aa094c1aae56d1
taras_svystun <tatarik.sv@gmail.com>
2024-04-20 23:42:15 +0300

    set right configs as from papers

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -143,9 +143,6 @@ class SourceSeparationDataset(Dataset):
         # )
         # create new mix segment
         mix_segment = tgt_segment.clone()
-        print('-' * 50)
-        print(self.TARGETS)
-        print('-' * 50)
         for target in self.TARGETS:
             # get random file to mix source from
             fp_template_to_add, indices_to_add = random.choice(self.filelist)
@@ -154,6 +151,10 @@ class SourceSeparationDataset(Dataset):
             )
             mix_segment += segment_to_add
             if target == self.target:
+                print('-' * 50)
+                print('-' * 50)
+                print('I am here')
+                print('-' * 50)
                 tgt_segment += segment_to_add
         return (
             mix_segment, tgt_segment
