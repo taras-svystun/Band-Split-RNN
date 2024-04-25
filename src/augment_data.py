eab7793cda9bf8c31c6f62f6f3496e820fd2aa59
taras_svystun <tatarik.sv@gmail.com>
2024-04-25 12:22:02 -0400

    prepared data augm script for supervisor gpu server

---
+++ src/augment_data.py
@@ -0,0 +1,14 @@
+from glob import glob
+import torchaudio
+from tqdm import tqdm
+import os
+from glob import glob
+from time import perf_counter
+import torch
+import torchaudio
+import torchaudio.transforms as T
+import torchaudio.functional as F
+import torch.nn.functional as TF
+from tqdm import tqdm
+
+for folder in glob('./')
